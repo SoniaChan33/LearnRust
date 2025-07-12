07707fd0835a7aa464975105769bb6bd9335ea36
sonia33 <soniachan33@126.com>
2025-07-12 09:26:50 +0800

    test function

--- src/ownership.rs
+++ src/ownership.rs
@@ -6,7 +6,16 @@ pub fn _ownership_example() {
     // println!("{}", s2);
 }
 
-pub fn take_ownership(s: &String) -> String {
+pub fn take_ownership(s: &String) -> &String {
     println!("Taking ownership of: {}", s);
-    s.clone() // 返回s的克隆
+    &s // 返回s的引用
+}
+
+#[test]
+fn test_take_ownership() {
+    let s1 = String::from("Hello, Rust!");
+    let s2 = take_ownership(&s1); // 传递s1的
+    // 引用而不是所有权
+    assert_eq!(s1, *s2); // 确保s1和s2的值相同
+    println!("s1: {}, s2: {}", s1, s2);
 }
