0a2d39cf808fffd6159464c612c4adedf7b3cec9
sonia33 <soniachan33@126.com>
2025-07-16 10:53:07 +0800

    hashmap example

--- src/collection.rs
+++ src/collection.rs
@@ -1,5 +1,6 @@
+use std::hash::DefaultHasher;
+use std::hash::Hash;
 use std::vec;
-
 #[test]
 fn vec_example() {
     // 创建一个空的vec
@@ -59,4 +60,119 @@ fn vec_example() {
         println!("{}", first);
     }
     v3.push(6);
+}
+
+use std::collections::HashMap;
+
+use serde_json::map;
+#[test]
+fn hash_map_example() {
+    // 基本操作
+    let mut scores: HashMap<String, i32> = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // 获取元素
+    let team_name = String::from("Blue");
+    let score: Option<&i32> = scores.get(&team_name);
+
+    match score {
+        Some(score) => println!("{}", score),
+        None => println!("None"),
+    }
+
+    // 遍历
+    for (key, value) in &scores {
+        println!("{} : {}", key, value);
+    }
+
+    // 更新
+    scores.insert(String::from("Blue"), 25);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    // 合并两个集合
+    let mut map1 = HashMap::new();
+    map1.insert(1, "one");
+    let mut map2 = HashMap::new();
+    map2.insert(2, "two");
+    map2.insert(3, "three");
+    for (k, v) in &map2 {
+        map1.entry(*k).or_insert(&v);
+    }
+    println!("{:?}", map1);
+}
+
+fn hashmap_ownership() {
+    let mut scores = HashMap::new();
+    let team_name = String::from("Blue");
+    let team_score = 10;
+    scores.insert(team_name.clone(), team_score.clone());
+    println!("{:?}", scores);
+
+    println!("{}", team_name);
+}
+
+/**
+ * 通过vec实现stack
+ */
+struct Stack<T> {
+    elements: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    // 初始化空栈
+    fn new() -> Self {
+        Stack {
+            elements: Vec::new(),
+        }
+    }
+
+    // 入栈：向尾部添加元素
+    fn push(&mut self, item: T) {
+        self.elements.push(item);
+    }
+
+    // 出栈：移除并返回尾部元素（空栈时返回 None）
+    fn pop(&mut self) -> Option<T> {
+        self.elements.pop()
+    }
+
+    // 查看栈顶：返回尾部元素的引用（空栈时返回 None）
+    fn peek(&self) -> Option<&T> {
+        self.elements.last()
+    }
+}
+#[test]
+// 测试用例
+fn main() {
+    let mut stack = Stack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    println!("栈顶元素（peek）: {:?}", stack.peek()); // 输出: Some(3)
+    println!("出栈元素（pop）: {:?}", stack.pop()); // 输出: Some(3)
+    println!("出栈后栈顶: {:?}", stack.peek()); // 输出: Some(2)
+}
+
+/**
+ * 通过hashmap计算单词频率
+ */
+
+fn count_frequency(s: &str) -> HashMap<&str, i32> {
+    let mut letters: HashMap<&str, i32> = HashMap::new();
+    for ch in s.split_whitespace() {
+        letters
+            .entry(ch)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+    return letters;
+}
+
+#[test]
+fn test_count_frequency() {
+    let s = "hello world hello rust";
+    let letters = count_frequency(s);
+    println!("{:?}", letters);
 }
